63fa7a65b15c5f277905a218372e29550d44b821
melondotdev <melonapplessss@gmail.com>
2024-04-10 14:09:23 -0700

    added home button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Login from "./Auth/Login";
 import UserOptions from "./UserOptions/UserOptions";
 import SuiSymbol from "../assets/sui-symbol.png";
+import * as IoIcons from "react-icons/io5";
 
 const Navbar = ({ walletData, isLoggedIn, isWalletConnected, setIsEditProfile, setIsMintJobPost }) => {
   const formatWalletAddress = (address) => {
@@ -14,13 +15,23 @@ const Navbar = ({ walletData, isLoggedIn, isWalletConnected, setIsEditProfile, s
   
   return (
     <div className="navbar flex justify-between items-center h-20 font-anton bg-transparent text-white z-10">
-      <Link to="/">
-        <div className="navbar-left flex items-center ml-4 text-3xl">
-          <span className="text-white">SUI</span>
-          <span className="text-ssblue">SNAILS</span>
-          <span className="text-white ml-2">LANCER</span>
-        </div>
-      </Link>
+      <div className="navbar-left flex items-center ml-4 text-3xl">
+        <a
+          href="https://www.suisnails.io"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="nav-menu-item ease-in-out duration-300 hover:text-ssblue"
+        >
+          <IoIcons.IoHomeOutline />
+        </a>
+        <Link to="/">
+          <div className="navbar-left flex items-center ml-4 text-3xl">
+            <span className="text-white">SUI</span>
+            <span className="text-ssblue">SNAILS</span>
+            <span className="text-white ml-2">LANCER</span>
+          </div>
+        </Link>
+      </div>
       <div className="navbar-right mr-4 flex">
         {!isLoggedIn ? (
             <Login />
